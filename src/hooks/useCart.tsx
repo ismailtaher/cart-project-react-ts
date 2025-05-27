dac488f46d4902b398fd5dcdd006dbc5ed00d49f
ismailtaher <ismail.taher@ymail.com>
2025-05-27 17:54:51 +0500

    add comments to explain logic useCart.tsx

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import CartContext from "../context/CartProvider";
 import { type UseCartContextType } from "../context/CartProvider";
 
+// Custom hook to encapsulate and return the current cart context value
 const useCart = (): UseCartContextType => {
   return useContext(CartContext);
 };
