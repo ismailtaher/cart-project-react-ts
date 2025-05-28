986842e7df7fdb8f8cecd7b752c993b4fa06e8a0
ismailtaher <ismail.taher@ymail.com>
2025-05-28 16:14:59 +0500

    add optimisation in Product.tsx

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,7 @@
 import type { ProductsType } from "../context/ProductsProvider"; // Product shape
 import type { ReducerActionType, ReducerAction } from "../context/CartProvider"; // For dispatch and action types
 import type { ReactElement } from "react";
+import { memo } from "react";
 
 // Props passed from ProductList
 type PropsType = {
@@ -48,4 +49,33 @@ const Product = ({
   return content;
 };
 
-export default Product;
+/**
+ * areProductsEqual is a custom comparison function used with React.memo.
+ * It ensures the Product component only re-renders if its 'product' or 'inCart' props change.
+ *
+ * 1. It compares each key of the previous product with the next one (shallow comparison).
+ * 2. Also checks if the 'inCart' boolean has changed.
+ * 3. If nothing has changed, the component won't re-render — optimizing performance when rendering many product items.
+ */
+function areProductsEqual(
+  { product: prevProduct, inCart: prevInCart }: PropsType,
+  { product: nextProduct, inCart: nextInCart }: PropsType
+) {
+  return (
+    Object.keys(prevProduct).every((key) => {
+      return (
+        prevProduct[key as keyof ProductsType] ===
+        nextProduct[key as keyof ProductsType]
+      );
+    }) && prevInCart === nextInCart
+  );
+}
+
+/**
+ * MemoizedProduct wraps the Product component using React.memo and a custom comparator.
+ * React.memo prevents re-renders of the Product component if the props haven't changed
+ * based on the logic defined in areProductsEqual.
+ */
+const MemoizedProduct = memo<typeof Product>(Product, areProductsEqual);
+
+export default MemoizedProduct;
